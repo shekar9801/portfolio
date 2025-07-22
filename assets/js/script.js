287c40313b60a3224d3f6ebb79965b8193103b64
shekar <guguloth.rajashekhar@talentpace.com>
2025-07-22 17:58:36 +0530

    updated code

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "Machine Learning", "web development","Freelancer"],
+    strings: ["frontend development", "backend development", "Machine Learning", "Data Science"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
